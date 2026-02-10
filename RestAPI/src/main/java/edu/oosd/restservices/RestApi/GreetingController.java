ca43bc3c9fa37cfee9f36115af8095c20a81592a
Collin F <thedrunkninja98@gmail.com>
2026-02-10 14:26:47 -0500

    feat: Added status messages

--- RestAPI/src/main/java/edu/oosd/restservices/RestApi/GreetingController.java
+++ RestAPI/src/main/java/edu/oosd/restservices/RestApi/GreetingController.java
@@ -1,5 +1,7 @@
 package edu.oosd.restservices.RestApi;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +15,31 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/")
-    public String home() {
-        return String.format(template_home, "/");
+    public ResponseEntity<String> home() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(String.format(template_home, "/"));
     }
 
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    public ResponseEntity<Greeting> greeting(@RequestParam(defaultValue = "World") String name) {
+        Greeting bad = new Greeting(counter.incrementAndGet(), "Welcome to the bad realm");
+        if (name.equalsIgnoreCase("IM BAD GRAH")) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(bad);
+        }
+        Greeting forbid = new Greeting(counter.incrementAndGet(), "Welcome to the forbidden realm");
+        if (name.equalsIgnoreCase("IM FORBIDDEN GRAH")) {
+            return ResponseEntity
+                    .status(HttpStatus.FORBIDDEN)
+                    .body(forbid);
+        }
+        Greeting greeting = new Greeting(counter.incrementAndGet(), String.format(template, name));
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(greeting);
     }
 
 }
